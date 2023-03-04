43ad3037cf82396bd6e036178201f5ce283aa099
Pablo <pablolega7@gmail.com>
2023-03-04 14:01:16 +0100

    Create style of login and register page, Create layout.jsx and implementated in login and register Page

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { Button, Grid, TextField, Typography, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const RegisterPage = () => {
-    return (
-        <div>RegisterPage</div>
-    );
+  return (
+    <AuthLayout title="Register">
+      <form>
+        <Grid container>
+
+        <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField
+              label="Complete Name"
+              type="text"
+              placeholder="Complete Name"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="correo@gmail.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={ 12 }>
+              <Button variant="contained" fullWidth>
+                Create Account
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>Already Have an Account?</Typography>
+          <Link component={RouterLink} color="inherit" to="/auth/login">
+            Get Into
+          </Link>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
--- src/theme/purpleTheme.js
+++ src/theme/purpleTheme.js
@@ -4,10 +4,10 @@ import { red } from "@mui/material/colors";
 export const purpleTheme = createTheme({
   palette: {
     primary   : {
-      main: "#543884",
+      main: "#13A697",
     },
     secondary : {
-      main: "#543884",
+      main: "#13A697",
     },
     error     : {
       main: red.A400,
