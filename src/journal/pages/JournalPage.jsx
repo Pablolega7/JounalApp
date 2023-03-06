0b902a40e80990b19e8f00508c19b76911391e32
Pablo <pablolega7@gmail.com>
2023-03-06 13:26:00 +0100

    Craete Navbar and SideBar component

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournaLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius culpa sapiente distinctio voluptate dolorem voluptatem modi fuga voluptates laboriosam tempora. Delectus praesentium aliquid ducimus pariatur iusto quibusdam officia numquam necessitatibus.</Typography>
+    </JournalLayout>
   );
 };
